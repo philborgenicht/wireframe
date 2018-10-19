ee3f9c020156d6f7af482c6d250000b245b88554
Phillip Borgenicht <philborgenicht@gmail.com>
2018-10-19 15:01:07 -0700

    added files

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems1 = document.querySelectorAll('.carousel');
+  var instances1 = M.Carousel.init(elems1);
+});
